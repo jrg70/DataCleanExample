77c1029d18b09642da2ff13e0f0b6f17c3196c3b
Jan Gröls <90774019+jrg70@users.noreply.github.com>
2022-06-20 15:52:35 +0100

    Update dbn.py

--- dbn.py
+++ dbn.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+data = {}
+
+data_files = ["AP_2010.csv",
+    "ClassSize_201011.csv",
+    "Demogra_2012.csv",
+    "Grad_200510.csv",
+    "Directory_201415.csv",
+    "SAT_2012.csv"]
+
+for f in data_files:
+    d = pd.read_csv("{0}".format(f))
+    key_name = f.replace(".csv", "")
+    data[key_name] = d
