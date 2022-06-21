f227fbf52a7209b8979a34209b1433a91d29afe5
Jan Gröls <90774019+jrg70@users.noreply.github.com>
2022-06-21 09:50:11 +0100

    Update dbn.py

--- dbn.py
+++ dbn.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import re
 
@@ -14,8 +15,12 @@ def find_lon(loc):
     lon = coords[0].split(",")[1].replace(")", "").strip()
     return lon
 
+def first_char(dbn):
+    return dbn[0:2]
+
 if __name__ == '__main__': 
 
+## Cleaning the data and merge it together
 data = {}
 
 data_files = ["AP_2010.csv",
@@ -84,4 +89,45 @@ data["hs_directory"]["lon"] = data["hs_directory"]["Location 1"].apply(find_lon)
 data["hs_directory"]["lat"] = pd.to_numeric(data["hs_directory"]["lat"], errors="coerce")
 data["hs_directory"]["lon"] = pd.to_numeric(data["hs_directory"]["lon"], errors="coerce")
 
+# condense datasets for unique dbn
+# 1. class size: filter, group, aggregate, reindex and assign back to data
+data["class_size"] = data["class_size"][data["class_size"]["GRADE "] == "09-12"]
+data["class_size"] = data["class_size"][data["class_size"]["PROGRAM TYPE"] == "GEN ED"]
+data["class_size"] = data["class_size"].groupby('DBN').agg(np.mean)
+data["class_size"].reset_index(inplace = True)
 
+# 2. Demographics: filter
+data['demographics'] = data['demographics'][data['demographics']['schoolyear'] == 20112012]
+
+# 3. Graduation
+data["graduation"] = data["graduation"][data["graduation"]["Cohort"] == "2006"]
+data["graduation"] = data["graduation"][data["graduation"]["Demographic"] == "Total Cohort"]
+
+# convert to numeric values
+cols = ['AP Test Takers ', 'Total Exams Taken', 'Number of Exams with scores 3 4 or 5']
+for col in cols:
+    data["ap_2010"][col] = pd.to_numeric(data["ap_2010"][col], errors="coerce")
+    
+# MERGE data
+# Stragtegy: left  to preserve most SAT results
+combined = data["sat_results"]
+
+to_merge = ["ap_2010","graduation"]
+for m in to_merge:
+    combined = combined.mege(data[m], on='DBN', how='left')
+
+to_merge = ["class_size", "demographics", "survey", "hs_directory"]
+for m in to_merge:
+    combined = combined.merge(data[m], on="DBN", how="inner")
+    
+print(combined.shape, combined.head(5))
+
+# (imputation) fill Nans with mean and for columns with mean Nan 0
+combined = combined.fillna(combined.mean())
+combined = combined.fillna(0)
+
+# Analysation aid
+combined['school_dist'] = combined['DBN'].apply(reduce)
+
+## Analysis
+# Check correlations
